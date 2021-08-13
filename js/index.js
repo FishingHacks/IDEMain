ccc582572d19384446bb88d23050ec0b4d5c14c0
FishingHacks <63118479+FishingHacks@users.noreply.github.com>
2021-08-13 17:17:23 +0200

    a

--- js/index.js
+++ js/index.js
@@ -53,13 +53,12 @@ function load() {
         evt.stopPropagation();
         evt.preventDefault();
         evt.dataTransfer.dropEffect = 'copy';
-        $("#container").addClass("dragover");
-        $("#dragtext").addClass("dragtext");
+        $(".dragtext").addClass("dragtext--over");
+        $(".dragtext--over").removeClass("dragtext");
     }
 
     function resetdrag() {
         $("#container").removeClass("dragover");
-        $("#dragtext").removeClass("dragtext");
     }
 
     document.getElementById("container").addEventListener('dragover', handleDragOver, false);
