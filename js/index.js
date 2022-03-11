3510b3932564825a7f1929653067da74b90e7fe7
FishingHacks <thejominingcraft@gmail.com>
2022-03-11 13:16:26 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
 var editorsettings = { window: { title: "Editor", file: "untitled", isSaved: false, theme: 0, themes: [vs, vsdark, hcblack] } };
+function integer_to_roman(num) {
+if (typeof num !== 'number') 
+return false; 
 
+var digits = String(+num).split(""),
+key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+"","I","II","III","IV","V","VI","VII","VIII","IX"],
+roman_num = "",
+i = 3;
+while (i--)
+roman_num = (key[+digits.pop() + (i * 10)] || "") + roman_num;
+return Array(+digits.join("") + 1).join("M") + roman_num;
+}
 function updateWindow() {
     document.title = editorsettings.window.title + " - " + editorsettings.window.file + (editorsettings.window.isSaved ? "" : "*");
 }
