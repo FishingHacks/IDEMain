8ae4b29f0151e92706e957d3eeb80bd5c66bfadc
FishingHacks <thejominingcraft@gmail.com>
2022-03-11 12:45:31 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -14,6 +14,34 @@ function load() {
     $("svg.fa-upload").mousedown(function () {
         $("#file").click();
     });
+    
+    //Registering snippets for HTML
+    monaco.languages.registerCompletionItemProvider('html', {
+	    provideCompletionItems: function (model, position) {
+		    // find out if we are completing a property in the 'dependencies' object.
+		    var textUntilPosition = model.getValueInRange({
+			    startLineNumber: 1,
+			    startColumn: 1,
+			    endLineNumber: position.lineNumber,
+			    endColumn: position.column
+		    });
+		    var word = model.getWordUntilPosition(position);
+		    var range = {
+			    startLineNumber: position.lineNumber,
+			    endLineNumber: position.lineNumber,
+			    startColumn: word.startColumn,
+			    endColumn: word.endColumn
+		    };
+		    return {
+			    suggestions: [
+                    {label: 'h1', kind: monaco.languages.CompletionItemKind.Function, documentation: 'Insert an h1-tag', range: range, insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, insertText: '<h1>${1:}</h1>'},
+                    {label: 'h2', kind: monaco.languages.CompletionItemKind.Function, documentation: 'Insert an h2-tag', range: range, insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, insertText: '<h2>${1:}</h2>'},
+                    {label: 'h3', kind: monaco.languages.CompletionItemKind.Function, documentation: 'Insert an h3-tag', range: range, insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, insertText: '<h3>${1:}</h3>'},
+                    {label: '!', kind: monaco.languages.CompletionItemKind.Function, documentation: 'Insert a basic document<br /><code>&lt;!DOCTYPE html&gt;\n&lt;html lang="en"&gt;\n&lt;head&gt;\n    &lt;meta charset="UTF-8"&gt;\n    &lt;meta http-equiv="X-UA-Compatible" content="IE=edge"&gt;\n    &lt;meta name="viewport" content="width=${1:device-width}, initial-scale=${2:1.0}"&gt;\n    &lt;title&gt;${3:Document}&lt;/title&gt;\n&lt;/head&gt;\n&lt;body&gt;\n    ${4: }\n&lt;/body&gt;\n&lt;/html&gt;</code>', range: range, insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet, insertText: '<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <meta http-equiv="X-UA-Compatible" content="IE=edge">\n    <meta name="viewport" content="width=${1:device-width}, initial-scale=${2:1.0}">\n    <title>${3:Document}</title>\n</head>\n<body>\n    ${4: }\n</body>\n</html>'},
+                ]
+		    };
+	    }
+    });
 }
 
 document.getElementById("container").addEventListener("drop", (e) => {
